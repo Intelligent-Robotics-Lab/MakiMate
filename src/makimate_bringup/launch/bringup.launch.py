b792113e6eb9a05878437ab5b3b553112c0fc309
TheBariumOxide <150753783+TheBariumOxide@users.noreply.github.com>
2025-11-20 15:38:47 -0500

    Delete src/makimate_bringup directory
    Not needed, saved a local copy in case changes want to be reverted

--- src/makimate_bringup/launch/bringup.launch.py
+++
@@ -1,7 +0,0 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(package='makimate_bringup', executable='hello', output='screen')
-    ])
